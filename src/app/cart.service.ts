a5a778444c668c710108ae0ffe70bfb2bd72542d
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-04-10 06:38:31 -0400

    created a cart service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  constructor() {}
+
+  items: Product[] = [];
+
+  // Add an item to cart
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  // Get all items in the cart
+  getItems() {
+    return this.items;
+  }
+
+  // Remove all items from the cart
+  clearCart() {
+    return (this.items = []);
+  }
+}
